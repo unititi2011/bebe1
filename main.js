78818c7f3a77efd51375fb7de89fad5f13346bf6
unititi2011 <107140140+unititi2011@users.noreply.github.com>
2023-05-10 14:30:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+som="";
+status2 ="";
+objetos=[];
+objetodetectato="";
+function preload() {
+    som=loadSound("buzzer_alarm.mp3")
+}
+function setup() {
+    canvas=createCanvas(380,380);
+    canvas.center();
+video=createCapture(VIDEO);
+video.size(380,380);
+video.hide();
+    
+}
+function inicio() {
+    objetodetectato=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("sta").innerHTML="Status detectando objetos"
+}
+function modelLoaded() {
+    console.log("modeloCarregado")
+    status2="true";
+    objetodetectato.detect(img,gotResult);
+}
+function draw() {
+    image(video,0,0,380,380);
+    if (status2!=" ") {
+        r=random(255);
+        g=random(255);
+        b=random(255);
+        objetodetectato.detect(video,gotResult);
+        for(i=0;i<objetos.length;i++){
+            document.getElementById("sta").innerHTML="Status objetos detectados"
+            fill(r,g,b);
+            porcentagem=floor(objetos[i].confidence*100);
+            text(objetos[i].label+" "+porcentagem +"%"+objetos[i].x+15+objetos[i].y+15);
+        }
+    }
+    }
+function gotResult(error,results) {
+    if(error){
+        console.error(error);
+    }
+    else{console.log(results);
+    objetos=results;
+    }
+}
